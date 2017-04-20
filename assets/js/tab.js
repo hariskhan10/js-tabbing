e46f2eb8b02252b9475751cd248d9557e9d94fff
harishasankhan <haris.khan@tajawal.com>
2017-04-20 13:27:00 +0400

    simple javascript tabbing

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -4,15 +4,15 @@
 
 $( document ).ready(function() {
     // Travel Offer Tabs start
-    $(".trv-tab-box > ul > li:not(:first)").hide();
-    $(".trv-tab-list > ul > li:first-child").addClass('active');
-    $(".trv-tab-list > ul > li").click(function () {
-        var index = $(".trv-tab-list > ul > li").index(this) + 1;
-        var tabselc = $(".trv-tab-box li:nth-child(" + index + ")");
+    $(".tab-box > ul > li:not(:first)").hide();
+    $(".tab-list > ul > li:first-child").addClass('active');
+    $(".tab-list > ul > li").click(function () {
+        var index = $(".tab-list > ul > li").index(this) + 1;
+        var tabselc = $(".tab-box li:nth-child(" + index + ")");
         tabselc.fadeIn();
         tabselc.siblings().hide();
         $(this).siblings().removeClass("active");
         $(this).addClass("active");
     });
-    
+
 });
